bc92a0ae00ac581dd13debd7058fd9d2561bb1a6
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2024-01-26 15:39:27 +0200

    error

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,12 @@
+const { HttpError } = require("../helpers/index.js");
+
+const isEmptyBody = async (req, res, next) => {
+    const keys = Object.keys(req.body);
+
+    if (!keys.length) {
+        return next(HttpError(400, "Body must have fields"));
+    }
+    next();
+};
+
+module.exports = isEmptyBody;
--- models/userModel.js
+++ models/userModel.js
@@ -1,30 +1,60 @@
+
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
+const  {handleSaveError, preUpdate} =require("./hooks.js");
+
 const { emailRegex } = require("../constant/constant");
 
-const userSchema = new Schema(
-  {
-    name: {
+const userSchema = new Schema({
+  username: {
       type: String,
-      default: null,
-    },
-    email: {
-      type: String,
-      // match: emailRegex,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    password: {
-      type: String,
-      minlength: 8,
-      required: [true, "Password is required"],
-    },
+      required: true,
   },
-  { versionKey: false }
-);
+  email: {
+      type: String,
+      match: emailRegex,
+      unique: true,
+      required: true,
+  },
+  password: {
+      type: String,
+      minLength: 8,
+      maxLength:64,
+      required: true,
+  },
+  token: {
+      type: String,
+  },
+  verify: {
+      type: Boolean,
+      default: false,
+  },
+  verificationCode: {
+      type: String,
+  }
+}, {versionKey: false, timestamps: true});
 
-//TODO:  add Joi validation
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", preUpdate);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+exports.userSignupSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().pattern(emailRegex).required(),
+  password: Joi.string().min(6).required(),
+});
+
+exports.userSigninSchema = Joi.object({
+  email: Joi.string().pattern(emailRegex).required(),
+  password: Joi.string().min(6).required(),
+});
+
+exports.userEmailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegex).required(),
+});
 
 const User = model("User", userSchema);
 
