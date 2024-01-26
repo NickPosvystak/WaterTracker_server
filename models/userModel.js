afbbcf56791d068af2fb9888cf874eb9bca6ea8b
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-01-26 09:34:49 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -24,21 +24,7 @@ const userSchema = new Schema(
   { versionKey: false }
 );
 
-// const userJoiSchema = Joi.object({
-//   password: Joi.string().min(8).max(48).required().messages({
-//     "string.base": `Password - should be a string type!`,
-//     "string.empty": `Password - cannot be an empty!`,
-//     "string.min": `Password - should have a minimum 8 symbols length!`,
-//     "string.max": `Password should have a maximum 48 symbols length!`,
-//     "any.required": `Password is a required!`,
-//   }),
-//   email: Joi.string().pattern(emailRegex).required().messages({
-//     "string.bae": `Email - should be a string type!`,
-//     "string.empty": `Email - cannot be an empty!`,
-//     "any.required": `Email is a required!`,
-//     "string.pattern.base": `Email - invalid pattern!`,
-//   }),
-// });
+//TODO:  add Joi validation
 
 const User = model("User", userSchema);
 
