a84d026f092b2d6385067b37c5586734f476e28d
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-01-29 14:37:01 +0100

    Update constant.js

--- constant/constant.js
+++ constant/constant.js
@@ -1,3 +1,7 @@
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const gender = { male: "male", female: "female" };
 
-module.exports = {emailRegex};
+module.exports = {
+  gender,
+  emailRegex,
+};
