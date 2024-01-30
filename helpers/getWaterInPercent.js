35ccab01658b270d33a72904eaf54793db26ad53
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-01-30 12:51:07 +0100

    Update getWaterInPercent.js

--- helpers/getWaterInPercent.js
+++ helpers/getWaterInPercent.js
@@ -1,6 +1,10 @@
 const getWaterInPercent = (consumedWater, dailyWaterIntake) => {
-  
- if (consumedWater === 0 || dailyWaterIntake === 0) {
+
+  if (
+    consumedWater === 0 ||
+    dailyWaterIntake === 0 ||
+    dailyWaterIntake === null
+  ) {
     return 0;
   }
   return (consumedWater * 100) / dailyWaterIntake || null;
